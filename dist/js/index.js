913d7f93ecb6091fecf5f040739d693b63ff3474
“ <bear19950320@gmail.com>
2018-04-27 16:24:00 +0800

    测试提交16:23

---
+++ dist/js/index.js
@@ -0,0 +1,36 @@
+(function() {  //加载
+  var obj =  {};
+  /**
+   * 动态加载脚本函数
+   * @param url 要加载的脚本路径
+   * @param callback  回调函数
+   */
+  obj.loadScript = function(url,callback){
+    var doc = document;
+    var script = doc.createElement("script");
+    script.type = "text/javascript";
+    if(script.readyState){  //IE
+      script.onreadystatechange = function(){
+        if(script.readyState=="load"||script.readyState=="complete"){
+          script.onreadystatechange = null;
+          callback();
+        }
+      };
+    }else{
+      script.onload = function(){
+        callback();
+      };
+    }
+    script.src = url;
+    doc.getElementsByTagName("head")[0].appendChild(script);
+  };
+  var jsList = [
+    "./dist/layui/layui.all.js",
+    './dist/layui/lay/modules/layer.js'
+  ];
+  function callback(){
+      jsList.length?obj.loadScript(jsList.shift(),callback)
+        :(function(){time = null})();
+  }
+  var time = setTimeout(function(){obj.loadScript(jsList.shift(),callback)},25);
+})();
