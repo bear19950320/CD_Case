a0162fc3b2c585232cfbd272cd9cfd6d327299a9
“ <bear19950320@gmail.com>
2018-05-08 15:39:16 +0800

    五月八日提交

--- dist/js/common.js
+++ dist/js/common.js
@@ -55,6 +55,7 @@ var common = {
      */
     'form': function (obj, name, type, valOption, id, col) {
         var objLength, nameArr;
+        // 如果 name 不是数组是专为数组(是数组就不变)
         if (typeof name != Array) {
             nameArr = name.split(",")
             objLength = name.split(",").length;
@@ -62,7 +63,6 @@ var common = {
             objLength = name.length;
             nameArr = name;
         }
-        console.log(valOption)
         if (objLength > 0) {
             var html = '<form class="layui-row layui-form searchCon" id="formCom">'
             for (var i = 0; i < objLength; i++) {
@@ -77,6 +77,7 @@ var common = {
                             html += '<div class="formInput layui-col-lg10 layui-col-sm10 layui-col-md10 layui-col-md-offset1 layui-col-sm-offset1 layui-col-lg-offset1">';
                         }
                     } else {
+                        //  已本身的名字的长度 来判断渲染什么样的删除格 
                         if (objLength % 3 == 0) {
                             html += '<div class="layui-col-lg4 layui-col-sm4 layui-col-md4 formInput">';
                         } else if (objLength % 4 == 0) {
@@ -86,7 +87,7 @@ var common = {
                         }
                     }
                     // 填充label的内容
-                    html += '<label class="layui-form-label">' + nameArr[i] + ' </label>';
+                    html += '<label class="layui-form-label">' + nameArr[i] + ': </label>';
                     // 判断是否是复选框
                     if (typeof valOption[i] == 'object') {
                         if (valOption[i].length >= 4) {
@@ -98,63 +99,74 @@ var common = {
                         html += '<div class="layui-input-inline">';
                     }
                     $type = type[i];
-                    console.log("判断生成元素的类型------" + $type)
                     switch ($type) {
                         /*如果这是input框
                         */
                         case 'input': {
-                            html += ': <input type="text" id="' + id[i] + '"  name="' + id[i] + '" title="' + nameArr[i] + '"  placeholder="请输入' + nameArr[i] + '" />'
+                            html += '<input type="text" id="' + id[i] + '"  name="' + id[i] + '" title="' + nameArr[i] + '"  placeholder="请输入' + nameArr[i] + '" />'
                             break;
                         }
                         /*如果这是select框
                         **/
                         case 'select': {
-                            html += ' <select class="form-control" id="' + id[i] + '"  name="' + id[i] + '" placeholder="请选择' + nameArr[i] + '"  title="' + nameArr[i] + '">'
+                            html += '<select class="form-control" id="' + id[i] + '"  name="' + id[i] + '" placeholder="请选择' + nameArr[i] + '"  title="' + nameArr[i] + '">'
                             html += '<option value="0">全部</option>';
                             html += commFun.option(valOption[i]);
                             html += '</select>';
                             break;
                         }
+                        // switch(开关)渲染
                         case 'switch': {
-                            html += ': <input type="checkbox" id="' + id[i] + '"  name="' + id[i] + '" title="' + nameArr[i] + '" lay-skin="switch" lay-text="' + valOption[i] + '" />'
+                            html += '<input type="checkbox" id="' + id[i] + '"  name="' + id[i] + '" title="' + nameArr[i] + '" lay-skin="switch" lay-text="' + valOption[i] + '" />'
                             break;
                         }
+                        // 复选框的渲染
                         case 'primaryNew': {
-                            html += ' <div style="display:block;">' + commFun.primaryNew(valOption[i], 'yyyy-MM-dd HH:mm:ss', '2018-5-12') + '</div>';
+                            html += '<div style="display:block;">' + commFun.primaryNew(valOption[i], 'yyyy-MM-dd HH:mm:ss', '2018-5-12') + '</div>';
                             break;
                         }
+                        // 文本输入框类型
                         case 'textArea': {
                             html += '<textarea rows="1" cols="18" placeholder="请输入' + nameArr[i] + '"></textarea>'
                             break;
                         }
+                        // 时间格式
                         case 'date': {
-                            $$dateType = commType[valOption[i]];
-                            html += '<input type="text" class="layui-input" id="' + id[i] + '" name="' + id[i] + '" title="' + nameArr[i] + '" placeholder="请选择' + nameArr[i] + '" />'
+                            var $$dateType = comm.dateType[valOption[i]];
+                            var $$dataStyle = comm.dateStyle[valOption[i]];
+                            html += '<input type="text" class="layui-input _' + id[i] + '" id="' + id[i] + '" name="' + id[i] + '" title="' + nameArr[i] + '" placeholder="请选择' + nameArr[i] + '" />'
                             // 根据格式初始化 layuiDate格式
                             if (valOption[i] == 11) {
-                                commFun.layuiDate(id[i], commType[valOption[i]], '', commStyle[valOption[i]])
+                                commFun.layuiDate(id[i], $$dateType, '', $$dataStyle)// 1. class名字 2. 时间样式 3. 历史val 4. 时间的类型
                             } else if (valOption[i] == 12) {
-                                commFun.layuiDate(id[i], commType[valOption[i]], '', commStyle[valOption[i]])
+                                commFun.layuiDate(id[i], $$dateType, '', $$dataStyle)
                             } else if (valOption[i] == 13) {
-                                commFun.layuiDate(id[i], commType[valOption[i]], '', commStyle[valOption[i]])
+                                commFun.layuiDate(id[i], $$dateType, '', $$dataStyle)
                             } else if (valOption[i] == 14) {
-                                commFun.layuiDate(id[i], commType[valOption[i]], '', commStyle[valOption[i]])
+                                commFun.layuiDate(id[i], $$dateType, '', $$dataStyle)
                             } else if (valOption[i] == 15) {
-                                commFun.layuiDate(id[i], commType[valOption[i]], '', commStyle[valOption[i]])
+                                commFun.layuiDate(id[i], $$dateType, '', $$dataStyle)
                             }
                             break;
                         }
+                        // 枚举按钮传入ID 请求接口渲染 tree 
                         case 'treeInput': {
                             html += '<input type="text" id="' + id[i] + '"  name="' + id[i] + '" title="' + nameArr[i] + '"  placeholder="请选择' + nameArr[i] + '"  onclick="commFun.treeFun(id)" />'
-
+                            break;
                         }
                     }
-                    html += '</div></div>'
+                    html += '</div>';
+                    //  判断是否是switch类型 添加 辅助文字---(注解)
+                    if (type[i] != 'switch') {
+                        html += '<div class="layui-form-mid layui-word-aux">辅助文字</div>'
+                    }
+
+                    html += '</div>'
                 }
             }
             html += '</form>';
+            // 添加到传入的DOM里
             $(obj).html(html);
-
         }
     }
 }
@@ -194,12 +206,17 @@ var commFun = {
         }
         return html;
     },
+    /**
+     * layuiDate的渲染
+     *  @param {layuiDate} object
+     * 下面的name需要更改
+     */
     "layuiDate": function (id, style, val, type) {
         layui.use('laydate', function () {
             var laydate = layui.laydate;
             //执行一个laydate实例
             laydate.render({
-                elem: '#' + id //指定元素
+                elem: '._' + id //指定元素
                 , type: type
                 , format: style
                 , value: val
@@ -207,24 +224,29 @@ var commFun = {
             });
         });
     },
+    /**
+     * 树(tree)的渲染
+     * @param {id} string
+     * 下面的ID是请求接口数据渲染出tree
+     */
     "treeFun": function (id) {
-        console.log(id)
+        // 打开一个弹出框 赋一个带ID的身体
         layer.open({
             type: 1,
             content: '<div class="layui-form"><div id="xtree1" class="xtree_contianer"></div></div>' //这里content是一个普通的String
-            ,area: ['600px', '300px'],
+            , area: ['600px', '300px'],
             offset: 'auto',
-            btn:["确认","取消","清除"],
-            btnAlign :'c',
-            yes: function(index, layero){
+            btn: ["确认", "取消", "清除"],
+            btnAlign: 'c',
+            yes: function (index, layero) {
                 layer.msg("确认")
                 //按钮【按钮一】的回调
             },
-            btn2: function(index,layero){
+            btn2: function (index, layero) {
                 layer.msg("取消");
                 return false;
             },
-            btn3: function(index,layero){
+            btn3: function (index, layero) {
                 layuiXtree.prototype.render = function () {
                     var _this = this;
                     _this.Loading(_this._options);
@@ -233,7 +255,7 @@ var commFun = {
                 return false;
             }
         });
-       
+        //  已form表单渲染tree的样式 应该要推倒重做！ 
         layui.use(['form'], function () {
             var form = layui.form;
             $.ajax({
@@ -251,30 +273,34 @@ var commFun = {
                     });
                 },
                 error: function () {
-    
+
                 }
             });
-            //1、最基础的用法 - 直接绑定json
         });
 
     }
 }
 
-
-var commType = {
-    "10": "HH:mm",            //  时间类型
-    "11": "yyyy-MM-dd HH:mm:ss",            //	年月日类型时间
-    "12": "yyyy-MM-dd",             //	年月日类型
-    "13": "yyyy-MM",              //	年月类型
-    "14": "MM-dd",              //	月日类型
-    "15": "HH:mm:ss",            //	时间类型
+// 全局格式定义
+var comm = {
+    // date样式
+    dateType: {
+        "10": "HH:mm",                  //时:分
+        "11": "yyyy-MM-dd HH:mm:ss",   //年-月-日 时:分:秒
+        "12": "yyyy-MM-dd",           //年-月-日
+        "13": "yyyy-MM",             //年-月
+        "14": "MM-dd",              //月-日
+        "15": "HH:mm:ss",          //时:分:秒
+    },
+    // date类型
+    dateStyle: {
+        "10": "time",                    //时间类型
+        "11": "datetime",               //年月日类型时间
+        "12": "date",                  //年月日类型
+        "13": "month",                //年月类型
+        "14": "date",                //月日类型
+        "15": "time",               //时间类型
+    }
 };
-var commStyle = {
-    "10": "time",                    //  时间类型
-    "11": "datetime",     //	年月日类型时间
-    "12": "date",             //	年月日类型
-    "13": "month",               //	年月类型
-    "14": "date",                //	月日类型
-    "15": "time",
-}
+
 
